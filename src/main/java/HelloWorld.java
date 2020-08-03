b99aa21dd96532aa40224032b6587fee7f27b7b0
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-08-03 11:43:19 +0700

    create project helloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+
+public class HelloWorld {
+
+    public String sayHi() {
+        return "Hello World";
+    }
+
+    public String sayHi(String name) {
+        return "Hello "+ name;
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void sayHi_should_return_Hello_World(){
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHi();
+        assertEquals("Hello World", result); //ค่าที่เราคาดหวัง,สิ่งที่เราได้มา
+    }
+    @Test
+    public void sayHi_with_neeya_should_return_Hello_Neeya(){
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHi("neeya");
+        assertEquals("Hello neeya", result); //ค่าที่เราคาดหวัง,สิ่งที่เราได้มา
+    }
+
+    @Test
+    public void sayHi_with_Hasaneeya_should_return_Hello_hasaneeya(){
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHi("Hasaneeya");
+        assertEquals("Hello Hasaneeya", result); //ค่าที่เราคาดหวัง,สิ่งที่เราได้มา
+    }
+}

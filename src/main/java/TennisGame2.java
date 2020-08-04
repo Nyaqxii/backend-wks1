b33a482a96cd71981ccdb7afc049436469085b9e
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-08-04 13:01:38 +0700

    refactor p2

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -43,39 +43,21 @@ public class TennisGame2 {
         //p2
         if (p2Point > 0 && p1Point ==0)
         {
-            if (p2Point ==1)
-                p2Res = "Fifteen";
-            if (p2Point ==2)
-                p2Res = "Thirty";
-            if (p2Point ==3)
-                p2Res = "Forty";
-
+            p2Res = getResult(p2Point);
             p1Res = "Love";
             score = p1Res + "-" + p2Res;
         }
 
         if (p1Point > p2Point && p1Point < 4)
         {
-            if (p1Point ==2)
-                p1Res="Thirty";
-            if (p1Point ==3)
-                p1Res="Forty";
-            if (p2Point ==1)
-                p2Res="Fifteen";
-            if (p2Point ==2)
-                p2Res="Thirty";
+            p2Res = getResult(p2Point);
+            p1Res = getResult(p1Point);
             score = p1Res + "-" + p2Res;
         }
         if (p2Point > p1Point && p2Point < 4)
         {
-            if (p2Point ==2)
-                p2Res="Thirty";
-            if (p2Point ==3)
-                p2Res="Forty";
-            if (p1Point ==1)
-                p1Res="Fifteen";
-            if (p1Point ==2)
-                p1Res="Thirty";
+            p2Res = getResult(p2Point);
+            p1Res = getResult(p1Point);
             score = p1Res + "-" + p2Res;
         }
 
